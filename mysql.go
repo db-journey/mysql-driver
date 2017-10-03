b7c6484363af8d41bbecadc15c0c000f58e3c681
Scott J. Goldman <scottjg@umich.edu>
2017-10-03 15:33:05 -0700

    Remove use of mysql.MySQLWarnings
    It was removed from the upstream mysql package in
    https://github.com/go-sql-driver/mysql/pull/676

--- mysql.go
+++ mysql.go
@@ -89,9 +89,6 @@ func (driver *Driver) Close() error {
 func (driver *Driver) ensureVersionTableExists() error {
 	_, err := driver.db.Exec("CREATE TABLE IF NOT EXISTS " + tableName + " (version bigint not null primary key);")
 
-	if _, isWarn := err.(mysql.MySQLWarnings); err != nil && !isWarn {
-		return err
-	}
 	r := driver.db.QueryRow("SELECT data_type FROM information_schema.columns where table_name = ? and column_name = 'version'", tableName)
 	dataType := ""
 	if err := r.Scan(&dataType); err != nil {
