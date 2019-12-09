fe33a02a6cbb804f6b834aa0f09eff2aeba32745
Philippe Lafoucrière <plafoucriere@gitlab.com>
2019-12-09 10:54:16 -0500

    Use Migrate v2

--- mysql.go
+++ mysql.go
@@ -13,9 +13,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/db-journey/migrate/direction"
-	"github.com/db-journey/migrate/driver"
-	"github.com/db-journey/migrate/file"
+	"github.com/db-journey/migrate/v2/direction"
+	"github.com/db-journey/migrate/v2/driver"
+	"github.com/db-journey/migrate/v2/file"
 	"github.com/go-sql-driver/mysql"
 )
 
